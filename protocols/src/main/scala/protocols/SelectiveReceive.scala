7c8ca160a1dd314281631fdbd0debd3741ec7e62
Joshua Junqueira <joshua.junqueira@gmail.com>
2019-04-19 16:52:36 -0500

    WIP

--- protocols/src/main/scala/protocols/SelectiveReceive.scala
+++ protocols/src/main/scala/protocols/SelectiveReceive.scala
@@ -1,6 +1,6 @@
 package protocols
 
-import akka.actor.typed._
+import akka.actor.typed.{Behavior, ExtensibleBehavior}
 import akka.actor.typed.scaladsl._
 
 object SelectiveReceive {
@@ -16,5 +16,24 @@ object SelectiveReceive {
       * Hint: Implement an [[ExtensibleBehavior]], use a [[StashBuffer]] and [[Behavior]] helpers such as `start`,
       * `validateAsInitial`, `interpretMessage`,`canonicalize` and `isUnhandled`.
       */
-    def apply[T](bufferSize: Int, initialBehavior: Behavior[T]): Behavior[T] = ???
+    def apply[T](bufferSize: Int, initialBehavior: Behavior[T]): Behavior[T] = new ExtensibleBehavior[T]() {
+      
+      import Behavior.{start, canonicalize, validateAsInitial, interpretMessage, isUnhandled}
+
+      val buffer = StashBuffer[T](bufferSize)
+
+      def receive(ctx: akka.actor.typed.ActorContext[T],msg: T): Behavior[T] = {
+          val started = validateAsInitial(start(initialBehavior, ctx))
+          val next = interpretMessage(started, ctx, msg)
+          if(isUnhandled(next)) {
+            buffer.stash(msg)
+            Behaviors.same
+          } else {
+            buffer.unstashAll(ctx.asScala, next)
+          }
+      }
+
+      def receiveSignal(ctx: akka.actor.typed.ActorContext[T],msg: akka.actor.typed.Signal): Behavior[T] = Behaviors.unhandled
+
+    }
 }
